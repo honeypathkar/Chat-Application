45a597383d96333e43dee59a285095c1f11fb30d
honeypatkar <honeypatkar70@gmail.com>
2024-07-04 13:33:52 +0530

    Searching emplementation if user exist show them

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
+import { where, collection, query, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
 
 export default function SearchBox() {
+  const [userName, setUserName] = useState("");
+  const [user, setUser] = useState(null);
+  const [err, setErr] = useState(false);
+
+  const handleSearch = async () => {
+    const q = query(
+      collection(db, "users"),
+      where("displayName", "==", userName)
+    );
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+      });
+    } catch (error) {
+      setErr(true);
+    }
+  };
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch();
+  };
+
   return (
     <>
       <div className="flex justify-center">
@@ -10,8 +34,24 @@ export default function SearchBox() {
           name="text"
           placeholder="Search..."
           type="search"
+          onChange={(e) => setUserName(e.target.value)}
+          onKeyDown={handleKey}
         ></input>
       </div>
+      {err && <span>Something went wrong</span>}
+      {user && (
+        <div className="flex pl-7 py-4 items-center  hover:bg-gray-800 hover:text-white">
+          <img
+            src={user.photoURL}
+            alt="User Profile"
+            className="w-10 h-10 rounded-full mr-3"
+          />
+          <div>
+            <h6 className="userName font-bold ">{user.displayName}</h6>
+          </div>
+        </div>
+      )}
+
       <div className="flex justify-center">
         <div className="border-b-2 border-gray-300 mt-3 w-[90%]"></div>
       </div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
@@ -18,7 +17,6 @@ const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 export const auth = getAuth();
 export const storage = getStorage();
 export const db = getFirestore();
